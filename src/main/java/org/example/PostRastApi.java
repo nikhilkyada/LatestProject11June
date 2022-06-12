484b677c49e86dd34f04a0fe269c9361435eaa6a
Nikhil Kyada <101011749+nikhilkyada@users.noreply.github.com>
2022-06-12 17:12:08 +0100

    Prectical with Rest Api

--- src/main/java/org/example/PostRastApi.java
+++ src/main/java/org/example/PostRastApi.java
@@ -63,6 +63,43 @@ public class PostRastApi {
         Assert.assertEquals(title, "sunt aut facere repellat provident occaecati excepturi optio reprehenderit");
     }
 
+    //Get Comment and EmailID (Nikhil)
+    @Test
+    public void when_passing_id_should_retuns_data(){
+        Response response = given()
+                .when()
+                .get("comments?postId=1")
+                .then()
+                .extract().response();
+
+        JsonPath jsonPath = response.jsonPath();
+        String email = jsonPath.getString("email");
+        System.out.println("Extract Email from comment 1 >>>>>>>"+ email);
+        Assert.assertEquals(email,"[Eliseo@gardner.biz, Jayne_Kuhic@sydney.com, Nikita@garfield.biz, Lew@alysha.tv, Hayden@althea.biz]","Email is not matched");
+
+    }
+
+    // Get name and email
+    @Test
+    public void quarry_param(){
+        Response response = given()
+                .when()
+                .queryParam("postId",2)
+                .get("comments")
+                .then()
+                .extract().response();
+
+        JsonPath jsonPath = response.jsonPath();
+        String name = jsonPath.getString("name");
+        String email = jsonPath.getString("email");
+        System.out.println(name);
+        System.out.println(email);
+
+        Assert.assertEquals(name,"[et fugit eligendi deleniti quidem qui sint nihil autem, repellat consequatur praesentium vel minus molestias voluptatum, et omnis dolorem, provident id voluptas, eaque et deleniti atque tenetur ut quo ut]","NAme is not matched");
+        Assert.assertEquals(email,"[Presley.Mueller@myrl.com, Dallas@ole.me, Mallory_Kunze@marie.org, Meghan_Littel@rene.us, Carmen_Keeling@caroline.name]","Emails is not matched");
+
+    }
+
     // Scenario Post to create
     @Test
     public void it_should_create_new_post() {
@@ -87,6 +124,29 @@ public class PostRastApi {
         Assert.assertEquals(title, "abcd");
         Assert.assertEquals(body, "abce efg");
     }
+
+    //Post Scenario
+    @Test
+    public void post_scenario(){
+        Response response = given()
+                .contentType(ContentType.JSON)
+                .when()
+                .body("{\n" +
+                        "\"postId\": 1,\n" +
+                        "\"name\": \"Jon Doe\",\n" +
+                        "\"email\": \"jon.doe@amazon.com\",\n" +
+                        "\"body\": \"this is jon doe\"\n" +
+                        "}\n")
+                .post("comments/")
+                .then()
+                .extract()
+                .response();
+
+        response.prettyPrint();
+        Assert.assertEquals(response.getStatusCode(),201,"Failed");
+
+    }
+
 
     // Scenario Put to create
     @Test
